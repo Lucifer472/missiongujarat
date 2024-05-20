ce1bb664668a743134db11780ce9f518d4db8cce
Lucifer472 <JanvI@0427>
2024-05-20 17:49:02 +0530

    fix url issue

--- constant/index.ts
+++ constant/index.ts
@@ -27,13 +27,14 @@ export const faq = {
   ],
 };
 
-export const byFile = "https://images.modsverse.com/upload/blog.php";
-export const byUrl = "https://images.modsverse.com/upload/url.php";
-export const byProfile = "https://images.modsverse.com/upload/profile.php";
+export const byFile = "https://images.drivingexamexpert.com/upload/blog.php";
+export const byUrl = "https://images.drivingexamexpert.com/upload/url.php";
+export const byProfile =
+  "https://images.drivingexamexpert.com/upload/profile.php";
 
-export const url = "https://modsverse.com";
+export const url = "https://modzverse.com";
 export const title = "ModVerse";
-export const description = `Welcome to modsverse.com, your go-to resource for comprehensive Driving Exam Blogs in Australia. Whether you're preparing for your learner's permit or aiming to ace your practical driving test, we provide valuable insights, expert tips, and firsthand experiences tailored to the Australian driving landscape. Navigate the road to success with our in-depth guides on driving test routes, regional road rules, and city-specific exam strategies. Stay updated on the latest changes in Australian driving regulations and boost your confidence behind the wheel. Join our community of learners and experienced drivers as we share the knowledge that drives success on the Australian roads. Start your journey to driving excellence today with modsverse.com!`;
+export const description = `Welcome to modzverse.com, your go-to resource for comprehensive Driving Exam Blogs in Australia. Whether you're preparing for your learner's permit or aiming to ace your practical driving test, we provide valuable insights, expert tips, and firsthand experiences tailored to the Australian driving landscape. Navigate the road to success with our in-depth guides on driving test routes, regional road rules, and city-specific exam strategies. Stay updated on the latest changes in Australian driving regulations and boost your confidence behind the wheel. Join our community of learners and experienced drivers as we share the knowledge that drives success on the Australian roads. Start your journey to driving excellence today with modzverse.com!`;
 export const keywords =
   "Driving exam tips, Learner's permit Australia, Australian driving test, Practical driving guide, Road test strategies, Driving lessons advice, Regional road rules, Australia driving experience, Driving test preparation, Aussie road safety, Driver education blog, Defensive driving skills, Australian driving laws, Driver's license journey, Traffic rules in Australia, Behind the wheel insights, Road test success stories, Driving confidence tips, Driving school recommendations, Australian road signs";
 export const CardImage = "/images";
