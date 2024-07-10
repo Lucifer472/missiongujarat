df5ac2f367f4c0e4a720ff64ec8263581b45b970
Lucifer472 <JanvI@0427>
2024-07-10 17:07:02 +0530

    asd

--- app/(main)/(auth)/admin/ads/page.tsx
+++ app/(main)/(auth)/admin/ads/page.tsx
@@ -5,7 +5,7 @@ import SwitchNav from "@/components/admin/SwitchNav";
 import ClientWrapper from "@/components/wrappers/client-wrapper";
 
 import { getAds } from "@/lib/ads";
-export const revalidate = 0;
+export const revalidate = 1;
 
 const AdChangePage = async () => {
   const ads = await getAds();
